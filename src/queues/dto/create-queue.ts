8e871add27f80857de244d134c4fb256c36da5d2
DevFelipreis <lfreis.contato@gmail.com>
2024-01-05 20:14:56 -0300

    fix:expert queue

---
+++ src/queues/dto/create-queue.ts
@@ -0,0 +1,6 @@
+import { IsNotEmpty } from 'class-validator';
+
+export default class CreateQueueDto {
+	@IsNotEmpty({ message: 'O campo expertId é obrigatório' })
+	expertId: string;
+}
--- src/queues/queues.controller.ts
+++ src/queues/queues.controller.ts
@@ -1,7 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+	BadRequestException,
+	Body,
+	Controller,
+	HttpStatus,
+	NotFoundException,
+	Post,
+	Res
+} from '@nestjs/common';
 import { QueuesService } from './queues.service';
+import CreateQueueDto from './dto/create-queue';
+import { ExpertsService } from 'src/experts/experts.service';
 
 @Controller('queues')
 export class QueuesController {
-	constructor(private readonly queuesService: QueuesService) {}
+	constructor(
+		private readonly queuesService: QueuesService,
+		private readonly expertsService: ExpertsService
+	) {}
+
+	@Post()
+	async createQueue(@Body() data: CreateQueueDto, @Res() res) {
+		const expertId = await this.expertsService.findOneExpert(data.expertId);
+
+		if (!expertId) {
+			throw new NotFoundException('O Expert informado não existe');
+		}
+
+		const queueExists = await this.queuesService.queueExpertExistsToday(
+			data.expertId
+		);
+
+		if (queueExists) {
+			throw new BadRequestException(
+				'O Expert informado está em uma fila de atendimento'
+			);
+		}
+
+		const queue = await this.queuesService.createQueue(data);
+
+		return res.status(HttpStatus.CREATED).json({ queue });
+	}
 }
